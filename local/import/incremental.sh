de9b04dadcbc47e56ccbb1f4ef21b9c49ef6e226
WWW Admin <www-admin@ip-10-10-2-18.ec2.internal>
2022-03-08 23:21:25 +0000

    Update gitignore to exclude private and instance-specific settings while adding some common config and import scripts. Closes #4.

---
+++ local/import/incremental.sh
@@ -0,0 +1,45 @@
+#
+# A helper script to automatically import bib and authorities
+
+# Redirect stdout ( > ) into a named pipe ( >() ) running "tee"
+#exec > >(tee -i logfile.txt)
+#exec 2>&1
+
+# set variables
+export VUFIND_HOME=/usr/local/vufind >> /etc/profile.d/vufind.sh
+export VUFIND_LOCAL_DIR=/usr/local/vufind/local >> /etc/profile.d/vufind.sh
+
+# vufind base path
+VUFIND_HOME=/usr/local/vufind
+
+# fails on the first error
+set -e
+
+today="$(date +'%d%m%Y')"
+printf "Record update started: %s\n" "$today"
+
+echo "import MARC records"
+php "$VUFIND_HOME"/harvest/harvest_oai.php
+"$VUFIND_HOME"/harvest/batch-delete.sh crl
+"$VUFIND_HOME"/harvest/batch-import-marc.sh crl
+
+# optimize index
+php "$VUFIND_HOME"/util/optimize.php
+echo "optimize index"
+
+# wait for solr to start
+su - vufind -c '"$VUFIND_HOME"/solr.sh restart'
+echo "restart solr index"
+
+# create browse index
+"$VUFIND_HOME"/index-alphabetic-browse.sh
+echo "create browse index"
+
+# remove caches
+rm -R "$VUFIND_HOME"/local/cache/searchspecs/*
+rm -R "$VUFIND_HOME"/local/cache/objects/*
+echo "remove caches"
+
+echo "finished"
+echo "####################################"
+exit 0 
