d05776c28a78fc7ee42aeae4de69e895dd0f7386
Ryan Jacobs <ryan.jake@gmail.com>
2022-05-05 17:22:34 -0500

    Add digitize note to format field display.

--- module/CRL/src/CRL/View/Helper/Root/RecordDataFormatterFactory.php
+++ module/CRL/src/CRL/View/Helper/Root/RecordDataFormatterFactory.php
@@ -14,13 +14,76 @@
      */
      public function getDefaultCoreSpecs()
      {
-        $spec = new SpecBuilder(parent::getDefaultCoreSpecs());
-        // Proof-of-concept for a custom field addition. May be removed upon
-        // further evaluation.
-        $spec->setLine('Key CRL Collections', 'getCRLScopes');
+        $spec = new \VuFind\View\Helper\Root\RecordDataFormatter\SpecBuilder();
+        $spec->setTemplateLine(
+            'Published in',
+            'getContainerTitle',
+            'data-containerTitle.phtml'
+        );
+        $spec->setLine(
+            'New Title',
+            'getNewerTitles',
+            null,
+            ['recordLink' => 'title']
+        );
+        $spec->setLine(
+            'Previous Title',
+            'getPreviousTitles',
+            null,
+            ['recordLink' => 'title']
+        );
+        $spec->setMultiLine(
+            'Authors',
+            'getDeduplicatedAuthors',
+            $this->getAuthorFunction()
+        );
+        $spec->setLine(
+            'Language',
+            'getLanguages',
+            null,
+            ['itemPrefix' => '<span property="availableLanguage" typeof="Language">'
+                           . '<span property="name">',
+             'itemSuffix' => '</span></span>']
+        );
+        $spec->setTemplateLine(
+            'Published',
+            'getPublicationDetails',
+            'data-publicationDetails.phtml'
+        );
+        $spec->setLine(
+            'Edition',
+            'getEdition',
+            null,
+            ['itemPrefix' => '<span property="bookEdition">',
+             'itemSuffix' => '</span>']
+        );
+        $spec->setTemplateLine('Series', 'getSeries', 'data-series.phtml');
+        $spec->setTemplateLine(
+            'Subjects',
+            'getAllSubjectHeadings',
+            'data-allSubjectHeadings.phtml'
+        );
+        $spec->setTemplateLine(
+            'child_records',
+            'getChildRecordCount',
+            'data-childRecords.phtml',
+            ['allowZero' => false]
+        );
+        $spec->setTemplateLine('Online Access', true, 'data-onlineAccess.phtml');
+        $spec->setTemplateLine(
+            'Related Items',
+            'getAllRecordLinks',
+            'data-allRecordLinks.phtml'
+        );
+        $spec->setTemplateLine('Tags', true, 'data-tags.phtml');
         // Add related links and continues.
         $spec->setTemplateLine(
             'Related Items', 'getAllRecordLinks', 'data-allRecordLinks.phtml'
+        );
+        $spec->setTemplateLine(
+            'Format',
+            'getFormats',
+            'format-list-msg.phtml'
         );
         return $spec->getArray();
      }
