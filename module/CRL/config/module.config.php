08aa9cff5bc35cab374f1c66926a64dca275d862
Ryan Jacobs <ryan.jake@gmail.com>
2022-06-10 17:57:17 -0500

    Initial route scaffolding.

--- module/CRL/config/module.config.php
+++ module/CRL/config/module.config.php
@@ -1,29 +1,63 @@
 <?php
 
-return [
-    'vufind' => [
-        'plugin_managers' => [
-            'ils_driver' => [
-                'factories' => [
-                    'CRL\\ILS\\Driver\\Folio' => 'VuFind\\ILS\\Driver\\FolioFactory',
-                ],
-                'aliases' => [
-                    'VuFind\\ILS\\Driver\\Folio' => 'CRL\\ILS\\Driver\\Folio',
-                ]
-            ],
-            'recorddriver' => [
-                'factories' => [
-                    'CRL\\RecordDriver\\SolrMarc' => 'VuFind\\RecordDriver\\SolrDefaultFactory'
-                ],
-                'aliases' => [
-                    'VuFind\\RecordDriver\\SolrMarc' => 'CRL\\RecordDriver\\SolrMarc'
-                ],
-                'delegators' => [
-                    'CRL\\RecordDriver\\SolrMarc' => [
-                        0 => 'VuFind\\RecordDriver\\IlsAwareDelegatorFactory',
-                    ],
-                ],
-            ],
-        ],
-    ],
-];
+return array (
+  'vufind' => 
+  array (
+    'plugin_managers' => 
+    array (
+      'ils_driver' => 
+      array (
+        'factories' => 
+        array (
+          'CRL\\ILS\\Driver\\Folio' => 'VuFind\\ILS\\Driver\\FolioFactory',
+        ),
+        'aliases' => 
+        array (
+          'VuFind\\ILS\\Driver\\Folio' => 'CRL\\ILS\\Driver\\Folio',
+        ),
+      ),
+      'recorddriver' => 
+      array (
+        'factories' => 
+        array (
+          'CRL\\RecordDriver\\SolrMarc' => 'VuFind\\RecordDriver\\SolrDefaultFactory',
+        ),
+        'aliases' => 
+        array (
+          'VuFind\\RecordDriver\\SolrMarc' => 'CRL\\RecordDriver\\SolrMarc',
+        ),
+        'delegators' => 
+        array (
+          'CRL\\RecordDriver\\SolrMarc' => 
+          array (
+            0 => 'VuFind\\RecordDriver\\IlsAwareDelegatorFactory',
+          ),
+        ),
+      ),
+    ),
+  ),
+  'router' => 
+  array (
+    'routes' => 
+    array (
+      'redirects' => 
+      array (
+        'type' => 'Laminas\\Router\\Http\\Segment',
+        'options' => 
+        array (
+          'route' => '/Controller/Action/:123',
+          'constraints' => 
+          array (
+            'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+            'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+          ),
+          'defaults' => 
+          array (
+            'controller' => 'Controller',
+            'action' => 'Action',
+          ),
+        ),
+      ),
+    ),
+  ),
+);
